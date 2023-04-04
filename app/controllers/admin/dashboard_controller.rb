068929f406b35550b39dbf693a5081a58bdeb604
Jenny Carroll <jennygc@telus.net>
2023-04-04 17:37:52 +0000

    add resources to dashboard route, fetch counts in dashboard_controller and store in instance variables. render counts in show.html.erb and link_to

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["BASIC_AUTH_USERNAME"], password: ENV["BASIC_AUTH_PASSWORD"]
   def show
+    @productCount = Product.count
+    @categoryCount = Category.count
   end
 end
