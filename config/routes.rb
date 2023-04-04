51d030ab5ec24fd30f8f9f8b9bb209a1ba2e53a6
Jenny Carroll <jennygc@telus.net>
2023-04-04 19:34:53 +0000

    create about controller and view using rails generator, modify route and link to it in top_nav and the footer in application.html

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
+
   resources :categories, only: [:show]
 
   resource :cart, only: [:show] do
@@ -19,6 +20,8 @@ Rails.application.routes.draw do
     root to: 'dashboard#show', resources: [:products, :categories]
     resources :products, except: [:edit, :update, :show]
   end
+
+  get '/about' => 'about#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
